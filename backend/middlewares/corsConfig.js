b7813e61a9f98bb9ba0e0fe51f97999cb4b0a5fe
Jacob Jerry Arackal <jacobjerryarackal123@gmail.com>
2026-02-25 12:54:51 +0530

    updated the vercel link

--- backend/middlewares/corsConfig.js
+++ backend/middlewares/corsConfig.js
@@ -1,7 +1,7 @@
 export const corsConfig = {
   origin: [
     'http://localhost:3000',           // Local Frontend
-    'https://secure-test-environment-enforcement-one.vercel.app/'     // Your future Vercel URL
+    'https://secure-test-environment-enforcement-one.vercel.app'     // Your future Vercel URL
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
